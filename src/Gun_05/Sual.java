a448c43fa29d18dbae974640fbc2783fea5321af
Alizamin <alizamin.work@gmail.com>
2026-04-27 20:57:28 +0400

    27 Apr Lesson and 1 Question Added

--- src/Gun_05/Sual.java
+++ src/Gun_05/Sual.java
@@ -1,9 +1,15 @@
 package Gun_05;
 
 import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class Sual extends BaseDriver {
 
+    @Test
     public void Test1(){
 
         // Yalniz CssSelector istifade olunsun
@@ -11,6 +17,22 @@ public class Sual extends BaseDriver {
         // Saytda bu bolmeye klikleyin "Basic Web Page"
         // Bu bolmede "Click Me" butonona klikleyin
         // Assert olaraq "You clicked the button!" yazisinin gorunduyunu tesdiqleyin
+
+        driver.get("https://testpages.eviltester.com/pages/basics/basic-web-page/");
+
+//        WebElement basicPage = driver.findElement(By.cssSelector("[class='td-sidebar-nav-active-item']"));
+//        basicPage.click();
+//        MyFunc.wait(2);
+
+        WebElement clickMeBtn = driver.findElement(By.cssSelector("[id='button1']"));
+        clickMeBtn.click();
+        MyFunc.wait(2);
+
+        WebElement clickMessage = driver.findElement(By.cssSelector("[id='click-message']"));
+
+        Assert.assertTrue("Uygun gelmedi" , clickMessage.getText().equals("You clicked the button!"));
+
+        gozleBagla();
 
     }
 }
