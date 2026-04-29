6feda7f8bf5830945230bf730f5bc68e3b99aa24
Alizamin <alizamin.work@gmail.com>
2026-04-29 20:13:35 +0400

    29 Apr 1 Answer Added

--- src/Gun_07/_01_XpathSelect_Sual.java
+++ src/Gun_07/_01_XpathSelect_Sual.java
@@ -1,7 +1,12 @@
 package Gun_07;
 
 import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class _01_XpathSelect_Sual extends BaseDriver {
     /*
@@ -15,5 +20,26 @@ public class _01_XpathSelect_Sual extends BaseDriver {
     @Test
     public void Test1(){
 
+        driver.get("https://www.ebay.com/");
+        MyFunc.wait(2);
+
+        WebElement selectMenu = driver.findElement(By.xpath("//select[@id='gh-cat']"));
+        Select javaSelect = new Select(selectMenu);
+
+        javaSelect.selectByValue("2984");
+        MyFunc.wait(2);
+
+        WebElement searchBtn = driver.findElement
+                (By.xpath("//span[@class='gh-search-button__label']"));
+        searchBtn.click();
+        MyFunc.wait(2);
+
+        WebElement headerText = driver.findElement
+                (By.xpath("//h1[@class='textual-display page-title']"));
+        System.out.println("headerText.getText() = " + headerText.getText());
+
+        Assert.assertTrue("Axtarilan soz tapilmadi", headerText.getText().contains("Baby"));
+
+        gozleBagla();
     }
 }
