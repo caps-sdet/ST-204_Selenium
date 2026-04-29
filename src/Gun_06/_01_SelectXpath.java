9e5b1caeb377ec8eb894f4209f7294a3d2dedca2
Alizamin <alizamin.work@gmail.com>
2026-04-29 19:42:40 +0400

    29 Apr 1 Question Added

--- src/Gun_06/_01_SelectXpath.java
+++ src/Gun_06/_01_SelectXpath.java
@@ -4,8 +4,13 @@ import Utility.BaseDriver;
 import Utility.MyFunc;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class _01_SelectXpath extends BaseDriver {
 
@@ -16,11 +21,13 @@ public class _01_SelectXpath extends BaseDriver {
         MyFunc.wait(2);
 
         WebElement selectMenu = driver.findElement(By.xpath("//select[@id='searchDropdownBox']"));
+//        WebElement selectMenu = wait.until(
+//                ExpectedConditions.visibilityOfElementLocated(By.xpath("//select[@id='searchDropdownBox']"))); Actions ile goreceyik
 //        selectMenu.click();
         Select javaSelect = new Select(selectMenu); // selectimiz selectMenu elementi uzerinde ishleyecek
 
-//        javaSelect.selectByIndex(2); // index ile dropdown menyudan secim etdik
-//        MyFunc.wait(2);
+        javaSelect.selectByIndex(2); // index ile dropdown menyudan secim etdik
+        MyFunc.wait(2);
 
         System.out.println("javaSelect.getOptions().size() = " + javaSelect.getOptions().size());
 //        for (int i = 0; i <javaSelect.getOptions().size() ; i++) {
@@ -35,9 +42,10 @@ public class _01_SelectXpath extends BaseDriver {
         System.out.println("selectMenu.isDisplayed() = " + selectMenu.isDisplayed());
         System.out.println("selectMenu.isEnabled() = " + selectMenu.isEnabled());
 
+        javaSelect.selectByValue("search-alias=computers-intl-ship"); //
         MyFunc.wait(2);
-        javaSelect.selectByValue("Computers");
-        MyFunc.wait(2);
+
+//        javaSelect.selectByVisibleText("Electronics");
 
         gozleBagla();
 
---
+++ src/Gun_07/_01_XpathSelect_Sual.java
@@ -0,0 +1,19 @@
+package Gun_07;
+
+import Utility.BaseDriver;
+import org.junit.Test;
+
+public class _01_XpathSelect_Sual extends BaseDriver {
+    /*
+    Test Ssenarisi
+    1. https://www.ebay.com/ saytina get
+    2. kateqoriyalardan "Baby" sec
+    3. Axtaris duymesine klikle
+    4. Gedilen sehifenin bash hissesinde "Baby" sozunun yazildigini dogrulayin
+    Locator ucun yalniz Xpath istifade edin, select ucun index ve ya value istifade ede bilersiz
+     */
+    @Test
+    public void Test1(){
+
+    }
+}
